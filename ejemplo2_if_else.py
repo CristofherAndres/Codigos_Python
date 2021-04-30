a7eea0b9b613ba7f961a4680cd303f75bceb4017
Cristofher <Cristofher.rojas@inacapmail.cl>
2021-04-30 01:36:40 -0400

    Ejercicios clase 5 - 29.04.2021

---
+++ ejemplo2_if_else.py
@@ -0,0 +1,9 @@
+#Datos de entrada
+semaforo = input("Ingrese el color del semaforo: ")
+semaforo = semaforo.upper()
+
+#Proceso y salida
+if semaforo == "VERDE":
+    print("Puedes cruzar la calle")
+else:
+    print("Esperar")
